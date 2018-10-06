89a8c53ca55ba778ff1770a68429b7bd8b1931d8
Ivan Crimson <crimzie@gmail.com>
2018-10-06 13:23:37 +0300

    cleanup

--- jvmclient/src/com/crimzie/workshop/services/ApiEndpoints.scala
+++
@@ -1,9 +0,0 @@
-package com.crimzie.workshop.services
-
-import com.crimzie.workshop.api.Api
-import endpoints.scalaj.client.{Endpoints, JsonEntitiesFromCodec}
-
-import scala.language.postfixOps
-
-class ApiEndpoints(val address: String)
-  extends Endpoints with Api with JsonEntitiesFromCodec
